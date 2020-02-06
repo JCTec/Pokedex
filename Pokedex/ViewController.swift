cb8e36a93b7aea1ee678b962006817a092e7b4e2
Juan Carlos Estevez <jc_estevezr@hotmail.com>
2020-02-06 14:53:34 +0100

    Initial State
    Created Pokedex

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Juan Carlos Estevez on 06/02/20.
+//  Copyright © 2020 Juan Carlos Estevez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
